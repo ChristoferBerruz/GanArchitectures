a7be282e8bd17e51834bb9e84828fd5e53cccdf3
Christofer Berruz <cberruzc@my.bridgeport.edu>
2020-12-21 21:49:25 -0500

    Main changed to represent new project structure

--- GanArchitectures/main.py
+++ GanArchitectures/main.py
@@ -1,1 +1,8 @@
+from utils.datarepository import DataRepository
+from models.gans import MLPGan
+from models.gans import DCGan
 
+repo = DataRepository.get_instance()
+data = repo.get_data("MNIST", 64, 1)
+Gan = DCGan(1)
+Gan.train(data, 100)
