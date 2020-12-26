0f2c94d432eb837c64e78dfd99c6b7c00b503c87
Christofer Berruz <cberruzc@my.bridgeport.edu>
2020-12-26 12:56:55 -0500

    WGANGP

--- GanArchitectures/models/gans.py
+++ GanArchitectures/models/gans.py
@@ -71,3 +71,32 @@ class WGanCP(WGanAbc):
 
     def gradient_penalty(self, real_images, fake_images):
         return None
+
+
+class WGanGP(WGanAbc):
+
+    def __init__(self, data):
+        channels = data.channels
+        self.latent_space = ModelConfig.latent_space
+        self.image_space = ModelConfig.image_space
+        self.G = DConvGenerator(channels, self.latent_space, self.image_space, nn.Tanh())
+        self.D = ConvDiscriminator(channels, self.image_space, nn.Identity())
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.name = "WGANGP"
+        self.ID = self.name + data.name
+        self.logger = Logger(self.ID)
+        lr = 1e-4
+        betas = (0.5, 0.999)
+        self.weight_clipping_limit = 0.01
+        self.d_optimizer = torch.optim.Adam(self.D.parameters(), lr=lr, betas=betas)
+        self.g_optimizer = torch.optim.Adam(self.G.parameters(), lr=lr, betas=betas)
+        self.critic_iter = 5
+        self.lambda_term = 10
+
+    def one_minus_one(self):
+        one = torch.FloatTensor([1])
+        minus_one = -1*one
+        return (minus_one, one)
+
+    def gradient_penalty(self, real_images, fake_images):
+        return None
